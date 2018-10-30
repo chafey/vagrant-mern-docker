b1dbfd4a8821b78ba7a89be2660245de42b79dbd
Chris Hafey <chafey@gmail.com>
2018-10-30 15:26:22 -0500

    added mongo

--- bootstrap.sh
+++ bootstrap.sh
@@ -61,3 +61,11 @@ apt-get install -y docker-compose
 
 # add vagrant user to docker group so it can access docker daemon 
 usermod -a -G docker vagrant
+
+# install mongodb 4.0 CE
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
+apt-get update
+apt-get install -y mongodb-org=4.0.3 mongodb-org-server=4.0.3 mongodb-org-shell=4.0.3 mongodb-org-mongos=4.0.3 mongodb-org-tools=4.0.3
+service mongod start
+
